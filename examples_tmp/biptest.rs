48c3b650958c4116767d06b000d083ed2e46a54b
vincentm65 <vincentmiranda65@gmail.com>
2026-06-21 19:25:12 -0400

    cleanup: remove catalog, plans, reviews; move task_list to test fixtures

--- examples_tmp/biptest.rs
+++
@@ -1,9 +0,0 @@
-#[tokio::main]
-async fn main() {
-    let r = tokio::task::spawn_blocking(|| {
-        tokio::task::block_in_place(|| {
-            tokio::runtime::Handle::current().block_on(async { 42 })
-        })
-    }).await;
-    println!("result: {:?}", r);
-}
